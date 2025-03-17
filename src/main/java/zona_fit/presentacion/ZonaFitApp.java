cce62268de8ce52846c918d106588597a6d8347c
Nahuel Clauser <clausernahuel@gmail.com>
2025-03-17 19:15:47 -0300

    Opciones hechas

--- src/main/java/zona_fit/presentacion/ZonaFitApp.java
+++ src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -2,6 +2,7 @@ package zona_fit.presentacion;
 
 import zona_fit.datos.ClienteDAO;
 import zona_fit.datos.IClienteDAO;
+import zona_fit.dominio.Cliente;
 
 import java.util.Scanner;
 
@@ -47,6 +48,73 @@ public class ZonaFitApp {
                 System.out.println("--- Listado de cleintes---");
                 var clientes = clienteDAO.listarCLiente();
                 clientes.forEach(System.out::println);
+            }
+            case 2 -> { // buscar cleintes
+                System.out.println("--- Buscar clientes---");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                var cliente = new Cliente(idCliente);
+                var encontrado = clienteDAO.buscarClienteporId(cliente);
+                if(encontrado){
+                    System.out.println("Cliente encontrado:" + cliente);
+                }else{
+                    System.out.println("Cliente no encontrado:");
+                }
+            }
+            case 3 -> {//agregar cliente
+                System.out.println("--- Agregar cliente---");
+                System.out.println("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.println("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.println("Memebresia: ");
+                var membresia = Integer.parseInt(consola.nextLine());
+                //creamos el objeto
+                var cliente = new Cliente(nombre,apellido,membresia);
+                var agregado = clienteDAO.agregarCliente(cliente);
+                if(agregado){
+                    System.out.println("Cliente agregado: " + cliente);
+                }else{
+                    System.out.println("No agregado");
+                }
+            }
+
+            case 4 -> {
+                System.out.println("--- Modificar cliente---");
+                System.out.println("Id Cliente: ");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                System.out.println("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.println("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.println("Memebresia: ");
+                var membresia = Integer.parseInt(consola.nextLine());
+                //creamos el objeto
+                var cliente = new Cliente(idCliente,nombre,apellido,membresia);
+                var modificado = clienteDAO.modificarCliente(cliente);
+                if(modificado){
+                    System.out.println("Modificado: "+ cliente);
+                }else{
+                    System.out.println("No modificado");
+                }
+            }
+            case 5 ->{
+                System.out.println("---Eliminar cliente---");
+                System.out.println("--- Id cliente:---");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                var cliente = new Cliente(idCliente);
+                var eliminado = clienteDAO.eliminarCliente(cliente);
+                if(eliminado){
+                    System.out.println("Eliminado "+ idCliente);
+                }else{
+                    System.out.println("No eliminado.");
+                }
+            }
+            case 6 ->{
+                System.out.println("Hasta pronto");
+                salir = true;
+            }
+            default ->{
+                System.out.println("Opcion no reconocida");
             }
         }
         return salir;
